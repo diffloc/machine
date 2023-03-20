b4e37e4a578eac7adf6ab0122c39490b4f793dd5
diffloc <6632742+diffloc@users.noreply.github.com>
2023-03-20 17:12:25 -0500

    Update CoffeeMachine.java

--- CoffeeMachine.java
+++ CoffeeMachine.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class CoffeeMachine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        howMuchIngredients(scanner);
+        // howMuchIngredients(scanner);
+        fillMachine(scanner);
 
 
     }
@@ -23,5 +24,41 @@ public class CoffeeMachine {
         System.out.println((water * numCups) + " ml of water");
         System.out.println((milk * numCups) + " ml of milk");
         System.out.println((coffeeBeans * numCups) + " g of coffee beans");
+    }
+
+    public static void fillMachine(Scanner scanner) {
+        // Ingredients per cup:
+        final int water = 200; // milliliters
+        final int milk = 50; // milliliters
+        final int beans = 15; // grams
+
+        System.out.println("Write how many ml of water the coffee machine has:");
+        int amtWater = scanner.nextInt();
+        System.out.println("Write how many ml of milk the coffee machine has:");
+        int amtMilk = scanner.nextInt();
+        System.out.println("Write how many grams of coffee beans the coffee machine has:");
+        int amtBeans = scanner.nextInt();
+        System.out.println("Write how many cups of coffee you will need:");
+        int numCups = scanner.nextInt();
+
+        int cupsWater = amtWater / water;
+        int cupsMilk = amtMilk / milk;
+        int cupsBeans = amtBeans / beans;
+
+        int maxCups = Math.min(cupsWater, cupsMilk);
+        maxCups = Math.min(cupsBeans, maxCups);
+
+        if (numCups == maxCups) {
+            System.out.println("Yes, I can make that amount of coffee");
+        } else if (numCups < maxCups) {
+            System.out.printf("Yes, I can make that amount of coffee (and even %d more than that)", (maxCups - numCups));
+        } else {
+            System.out.printf("No, I can make only %d cup(s) of coffee", maxCups);
+        }
+
+
+
+
+
     }
 }
